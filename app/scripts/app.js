3bcf1c6f5f30d4b1d622a4197efefc36532c1297
zhzhang <xiaoyanzhuzzh@sina.com>
2015-04-29 18:14:54 +0800

    add register view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,6 @@
 'use strict';
-
+import jquery from 'jquery'
+import semantic from 'xiaoyanzhuzzh/Semantic-UI@master/dist/semantic.min'
 import angular from 'angular'
 import ngRoute from 'angular-route'
 
@@ -8,6 +9,9 @@ angular
   .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
       when('/', {
+        templateUrl: '.tmp/views/login.html'
+      }).
+      when('/register', {
         templateUrl: '.tmp/views/register.html'
       }).
       otherwise({
