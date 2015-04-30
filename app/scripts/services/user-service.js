e94d57bd140ec0d3b31a50f193b71e95e2234cab
zhzhang <xiaoyanzhuzzh@sina.com>
2015-04-30 14:25:42 +0800

    add service

---
+++ app/scripts/services/user-service.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+class UserService {
+
+  constructor($http) {
+
+    this.$http = $http
+  }
+
+  verify_email(email) {
+
+    let result = true;
+    let email_regular = /^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/;
+
+    if(!email_regular.exec(email)){
+
+      result = false
+    }
+
+    return result;
+  }
+}
+
+UserService.$inject = ['$http'];
+
+export { UserService }
