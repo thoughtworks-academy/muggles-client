400a044547e85101856fd560591409d43e2c3205
fgcui <fgcui1204@163.com>
2015-05-05 11:21:24 +0800

    modify loginController as vm

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,13 +4,17 @@ import semantic from 'xiaoyanzhuzzh/Semantic-UI@master/dist/semantic.min'
 import angular from 'angular'
 import ngRoute from 'angular-route'
 
+import { loginController } from './controllers/login-controller'
+
 angular
   .module('Muggles', ['ngRoute'])
+    .controller('loginController',loginController)
   .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
       when('/', {
         templateUrl: '.tmp/views/login.html',
-        controller: 'loginController'
+        controller: 'loginController',
+        controllerAs: 'vm'
       }).
       when('/register', {
         templateUrl: '.tmp/views/register.html'
