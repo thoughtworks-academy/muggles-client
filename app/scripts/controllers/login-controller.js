5cdfe9476a939d14cd557df617e79d4f48b764bb
fgcui <fgcui1204@163.com>
2015-05-05 11:21:41 +0800

    add two function

--- app/scripts/controllers/login-controller.js
+++ app/scripts/controllers/login-controller.js
@@ -1,3 +1,36 @@
-/**
- * Created by fgcui on 5/4/15.
- */
+'use strict';
+
+const EMAIL_IS_REQUIRED = '邮箱不能为空';
+const PASSWORD_IS_REQUIRED = '密码不能为空';
+
+class loginController{
+
+    constructor() {
+        this.email_required_signal = false;
+        this.email_is_required = EMAIL_IS_REQUIRED;
+
+        this.password_required_signal = false;
+        this.password_is_required = PASSWORD_IS_REQUIRED;
+    }
+    check_email(email) {
+        this.email_required_signal = false;
+
+        if(email === '') {
+            this.email_required_signal = true;
+        }
+    }
+
+    check_password(password) {
+        this.password_required_signal = false;
+
+        if(password === '') {
+            this.password_required_signal = true;
+        }
+    }
+    login(){
+        console.log('login...');
+    }
+}
+
+loginController.$inject = ['$scope'];
+export { loginController };
