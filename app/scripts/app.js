a12436bf2c6ad389ac6ad8faa7687ead6196d2b0
zhzhang <xiaoyanzhuzzh@sina.com>
2015-05-06 14:48:47 +0800

    rename some files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,8 +5,9 @@ import semantic from 'xiaoyanzhuzzh/Semantic-UI@master/dist/semantic.min'
 import angular from 'angular'
 import ngRoute from 'angular-route'
 
-import { UserService } from './services/user-service'
-import { RegisterController } from './controllers/register-controller'
+import { UserService } from './services/user'
+import { RegisterController } from './controllers/register'
+import { AppraiseController } from './controllers/appraise'
 
 
 angular
@@ -15,6 +16,7 @@ angular
   ])
   .service('userService', UserService)
   .controller('registerController', RegisterController)
+  .controller('appraiseController', AppraiseController)
   .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
       when('/', {
@@ -23,6 +25,11 @@ angular
       when('/register', {
         templateUrl: '.tmp/views/register.html',
         controller: 'registerController',
+        controllerAs: 'vm'
+      }).
+      when('/appraise', {
+        templateUrl: '.tmp/views/appraise.html',
+        controller: 'appraiseController',
         controllerAs: 'vm'
       }).
       otherwise({
