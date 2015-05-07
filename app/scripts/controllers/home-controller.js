8ce0dc4449ccbefedd4b257b4951af79bb10d87c
ximwang <wangximingde@163.com>
2015-05-07 09:30:38 +0800

    fix ng-repeat bug

--- app/scripts/controllers/home-controller.js
+++ app/scripts/controllers/home-controller.js
@@ -5,7 +5,6 @@ class HomeController {
     this.types = ['日', '周', '月', '季'];
     this.levels = ['A', 'B', 'C', 'D', 'X'];
     this.homeService = homeService;
-    this.trainees = 'fdsafa';
     homeService.get_trainee().then((trainees => {
       console.log(trainees.data);
       this.trainees = trainees.data;
