649c0b2546062dc825b2dea5f92beeb1c671838d
ximwang <wangximingde@163.com>
2015-05-07 16:46:15 +0800

    fix bind

--- app/scripts/controllers/home-controller.js
+++ app/scripts/controllers/home-controller.js
@@ -2,7 +2,7 @@
 
 class HomeController {
   constructor(homeService) {
-    this.types = ['日', '周', '月', '季'];
+    this.types = [{name:'日', value: 'date'}, {name: '周', value: 'week'}, {name: '月', value: 'month'}, {name: '季', value: 'season'}];
     this.levels = ['A', 'B', 'C', 'D', 'X'];
     this.homeService = homeService;
     this.trainee = {};
@@ -15,6 +15,25 @@ class HomeController {
   add_appraise(apparice, trainee) {
     console.log(apparice);
     this.homeService.add_appraise(apparice, trainee);
+  }
+
+  type_change(type){
+    switch (type) {
+      case 'date':
+            console.log('date');
+            break;
+      case 'week':
+            console.log('week');
+            break;
+      case 'month':
+            console.log('month');
+            break;
+      case 'season':
+            console.log('season');
+            break;
+      default :
+            console.log('nothing');
+    }
   }
 }
 
