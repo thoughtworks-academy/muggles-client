bed48887f154e7a01ab36c6ef89e7cfc768f5ce7
zhzhang <xiaoyanzhuzzh@sina.com>
2015-05-08 09:25:14 +0800

    complete correct dispaly

--- app/scripts/controllers/appraise-controller.js
+++ app/scripts/controllers/appraise-controller.js
@@ -5,6 +5,11 @@ class AppraiseController {
   constructor($routeParams, traineeService) {
 
     this.traineeService = traineeService;
+    this.types = ['日', '周', '月', '季'];
+    this.levels = ['A', 'B', 'C', 'D', 'X'];
+    this.appraise = {
+      appraised_date: new Date(2015, 4, 15)
+    };
 
     this.day_appraises_signal = true;
     this.week_appraises_signal = false;
@@ -15,8 +20,9 @@ class AppraiseController {
       .then(resp => {
 
         this.trainee_name = resp.data.username;
+        this.groups = resp.data.groups;
+        
         let appraises = resp.data.appraises;
-
         this.day_appraises = appraises.filter(appraise => appraise.type === '日');
         this.week_appraises = appraises.filter(appraise => appraise.type === '周');
         this.month_appraises = appraises.filter(appraise => appraise.type === '月');
@@ -24,6 +30,16 @@ class AppraiseController {
       })
   }
 
+  submit_appraise_information(appraise) {
+
+    //页面下拉菜单添加Group
+    //利用moment处理appraised_date时间格式
+    //利用mment生成create_date
+    //后台的session中获取trainer的id
+    //发送post请求创建新的评论
+    //然后利用appraise的id将其push到trainee的appraises中
+    console.log(appraise)
+  }
   click_day_appraises() {
 
     this.day_appraises_signal = true;
