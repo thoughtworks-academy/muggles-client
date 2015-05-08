6b03deaab457e8f292e0db46045c25200536eeb8
fgcui <fgcui1204@163.com>
2015-05-08 17:51:58 +0800

    modify index controller

--- app/scripts/controllers/index-controller.js
+++ app/scripts/controllers/index-controller.js
@@ -1,3 +1,30 @@
-/**
- * Created by fgcui on 5/8/15.
- */
+'use strict';
+
+class IndexController {
+
+  constructor (indexService,$location) {
+    this.indexService = indexService;
+    this.location = $location;
+
+    indexService.get_session().then((resp => {
+
+      if(resp.data === ''){
+        this.show_user_message_signal = false;
+        this.show_logout_signal = false;
+      }else{
+
+        this.show_user_message_signal = true;
+        this.show_user_name_signal = true;
+        this.show_logout_signal = true;
+        this.trainerName = resp.data;
+      }
+    }));
+  }
+
+  logout(){
+    this.indexService.logout().then(() => this.location.path('/login'))
+  }
+}
+
+IndexController.$inject = ['indexService','$location'];
+export { IndexController };
