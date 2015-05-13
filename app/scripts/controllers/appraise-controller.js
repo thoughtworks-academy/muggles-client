6ecee0c04b67e02f6047d23430bd63f3478ccabc
zhzhang <xiaoyanzhuzzh@sina.com>
2015-05-13 14:06:59 +0800

    can modify comment

--- app/scripts/controllers/appraise-controller.js
+++ app/scripts/controllers/appraise-controller.js
@@ -22,7 +22,7 @@ class AppraiseController {
     this.appraise = {
       appraised_date: new Date()
     };
-    this.rewrite_appraise_modal_signal = true;
+    this.rewrite_appraise_signal = false;
     this.day_appraises_signal = true;
     this.week_appraises_signal = false;
     this.month_appraises_signal = false;
@@ -74,20 +74,32 @@ class AppraiseController {
       create_date: moment().format('YYYY-MM-DD HH:mm:ss')
     };
 
-    this.traineeService.has_appraised(current_appraise, this.trainee_id)
+    this.traineeService.create_appraise(current_appraise, this.trainee_id)
       .then(resp => {
+        console.log(resp.data);
 
-        if(!resp.data) {
-          this.traineeService.add_appraise(current_appraise, this.trainee_id)
-            .then(result => {
-
-              console.log(result);
-            })
-        } else {
-          //message show
-          console.log('message show')
+        if(resp.data) {
+          this.rewrite_appraise_signal = true;
         }
       });
+  }
+
+  submit_rewrite_appraise(appraise) {
+
+    let current_appraise = {
+      level: appraise.level,
+      type: appraise.type,
+      comment: appraise.comment,
+      group: appraise.group._id,
+      appraiser: appraise.appraiser,
+      appraised_date: moment(appraise.appraised_date).format('YYYY-MM-DD HH:mm:ss'),
+      create_date: moment().format('YYYY-MM-DD HH:mm:ss')
+    };
+    //发送post请求覆盖之前的评论
+    this.traineeService.update_appraise(current_appraise, this.trainee_id)
+      .then(resp => {
+        console.log(resp.data);
+      })
   }
 
   click_day_appraises() {
